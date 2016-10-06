72af515e2f6dbded56e8bc296ce1d0ec8a8d0e55
RNewsom <ryanjnewsom@gmail.com>
2016-10-06 11:22:42 -0600

    Added a view

--- MVCApp/Controllers/HelloWorldController.cs
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -10,16 +10,19 @@ namespace MVCApp.Controllers
     public class HelloWorldController : Controller
     {
         // GET: HelloWorld
-        public String Index()
+        public ActionResult Index()
         {
-            return "This is my default action...";
+            return View();
         }
 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID is: " + ID);
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
         }
     }
 }
