a0a20ae341c231ed84320746207984282451463c
RNewsom <ryanjnewsom@gmail.com>
2016-10-06 10:57:58 -0600

    Added a controller

---
+++ MVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Util;
+
+namespace MVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public String Index()
+        {
+            return "This is my default action...";
+        }
+
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID is: " + ID);
+        }
+    }
+}
